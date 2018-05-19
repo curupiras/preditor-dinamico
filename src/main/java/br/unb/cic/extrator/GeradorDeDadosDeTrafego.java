7999d95fe159c37a755c144a552c94e14c3441b3
Curupiras <curupiras@gmail.com>
2018-05-18 21:38:28 -0300

    Fim da primeira versão do GeradorDeDadosDeTrafego

--- src/main/java/br/unb/cic/extrator/GeradorDeDadosDeTrafego.java
+++ src/main/java/br/unb/cic/extrator/GeradorDeDadosDeTrafego.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Point;
 
 import br.unb.cic.extrator.dominio.ElementoGrafo;
@@ -18,9 +21,14 @@ import br.unb.cic.extrator.dominio.localizacao.Localizacao;
 import br.unb.cic.extrator.dominio.localizacao.LocalizacaoRepository;
 import br.unb.cic.extrator.dominio.no.No;
 import br.unb.cic.extrator.dominio.no.NoRepository;
+import br.unb.cic.extrator.dominio.tempoviagem.TempoViagem;
+import br.unb.cic.extrator.dominio.tempoviagem.TempoViagemRepository;
+import br.unb.cic.parametros.Parametros;
 
 @Component
 public class GeradorDeDadosDeTrafego {
+
+	private static final Log logger = LogFactory.getLog(GeradorDeDadosDeTrafego.class);
 
 	@Autowired
 	private ArcoRepository arcoRepository;
@@ -30,6 +38,9 @@ public class GeradorDeDadosDeTrafego {
 
 	@Autowired
 	private LocalizacaoRepository localizacaoRepository;
+
+	@Autowired
+	private TempoViagemRepository tempoViagemRepository;
 
 	private List<Arco> arcos;
 	private List<No> nos;
@@ -61,10 +72,12 @@ public class GeradorDeDadosDeTrafego {
 
 	@Scheduled(initialDelay = 0, fixedRate = 60000)
 	public void scheduledTask() {
+		logger.info("Iniciando geração de dados de tráfego.");
 		List<String> listaOnibus = localizacaoRepository.findDistinctOnibus();
 		for (String onibus : listaOnibus) {
 			extrairDadosDeTrafegoPorOnibus(onibus);
 		}
+		logger.info("Fim da geração de dados de tráfego.");
 	}
 
 	private void extrairDadosDeTrafegoPorOnibus(String onibus) {
@@ -73,22 +86,178 @@ public class GeradorDeDadosDeTrafego {
 		encontraElementoGrafoParaCadaLocalizacao(localizacoes);
 
 		for (int i = 0; i < localizacoes.size(); i++) {
+			// TODO: Levar em consideração tempo dispendido na parada mesmo que
+			// o onibus passe de um arco para outro e não seja capturado nenhuma
+			// medida na parada
 			if (localizacoes.get(i).getElementoGrafo().getClass().equals(Arco.class)) {
+
 				List<Localizacao> trecho = new ArrayList<Localizacao>();
 				trecho.add(localizacoes.get(i));
-				while (localizacoes.get(i).getElementoGrafo() == localizacoes.get(i + 1).getElementoGrafo()) {
+
+				while (i + 1 < localizacoes.size()
+						&& localizacoes.get(i).getElementoGrafo() == localizacoes.get(i + 1).getElementoGrafo()) {
+
 					i = i + 1;
 					trecho.add(localizacoes.get(i));
 				}
-				extrairDadosDeTravegoDeTrecho(trecho);
-				// TODO: Parei aqui!
+
+				extrairDadosDeTrafegoDeTrecho(trecho);
+			} else {
+
+				List<Localizacao> parada = new ArrayList<Localizacao>();
+				int primeiroIndiceDaParada = i;
+				int ultimoIndiceDaParada = i;
+
+				parada.add(localizacoes.get(i));
+
+				while (i + 1 < localizacoes.size()
+						&& localizacoes.get(i).getElementoGrafo() == localizacoes.get(i + 1).getElementoGrafo()) {
+
+					i = i + 1;
+					parada.add(localizacoes.get(i));
+					ultimoIndiceDaParada = i;
+				}
+
+				Localizacao localizacaoAnterior;
+				Localizacao localizacaoPosterior;
+
+				if (primeiroIndiceDaParada == 0 || localizacoes.get(primeiroIndiceDaParada).getElementoGrafo()
+						.getAnterior() != localizacoes.get(primeiroIndiceDaParada - 1).getElementoGrafo()) {
+					localizacaoAnterior = null;
+				} else {
+					localizacaoAnterior = localizacoes.get(primeiroIndiceDaParada - 1);
+				}
+
+				if (ultimoIndiceDaParada == localizacoes.size() - 1
+						|| localizacoes.get(ultimoIndiceDaParada).getElementoGrafo().getProximo() != localizacoes
+								.get(ultimoIndiceDaParada + 1).getElementoGrafo()) {
+					localizacaoPosterior = null;
+				} else {
+					localizacaoPosterior = localizacoes.get(ultimoIndiceDaParada + 1);
+				}
+
+				extrairDadosDeTrafegoDeParada(parada, localizacaoAnterior, localizacaoPosterior);
 			}
 		}
 	}
 
-	private void extrairDadosDeTravegoDeTrecho(List<Localizacao> trecho) {
-		// TODO Auto-generated method stub
+	private void extrairDadosDeTrafegoDeParada(List<Localizacao> parada, Localizacao localizacaoAnterior,
+			Localizacao localizacaoPosterior) {
+		
+		if(localizacaoAnterior == null || localizacaoPosterior == null){
+			//TODO: Gravar null ou um valor padrão?
+			armazenarDadosDeTrafegoDaParada(parada, null);
+			return;
+		}
+		
+		long tempo = 0;
 
+		// Calcula tempo entre localizacoes da mesma parada
+		for (int i = 0; i < parada.size() - 1; i++) {
+			tempo = tempo + Math.abs(parada.get(i).getDataHora().getTime() - parada.get(i + 1).getDataHora().getTime())
+					/ 1000;
+		}
+
+		// Calcula tempo entre chegada do onibus e primeira medida na parada
+		if (localizacaoAnterior != null) {
+			Point pontoLocalizacaoAnterior = localizacaoAnterior.getGeoPto();
+			Geometry pontoDaParada = ((No) parada.get(0).getElementoGrafo()).getPonto();
+			// TODO:Calcular essa distância andando sobre a linha e não em linha
+			// reta
+			double distancia = pontoLocalizacaoAnterior.distance(pontoDaParada);
+			double velocidade = localizacaoAnterior.getVelocidade()
+					* Parametros.KILOMETROS_POR_HORA_PARA_METROS_POR_SEGUNDO;
+			double periodo = distancia / velocidade;
+
+			long tempoLocalizacaoAnteriorParada = Math
+					.abs(parada.get(0).getDataHora().getTime() - localizacaoAnterior.getDataHora().getTime()) / 1000;
+
+			long diferenca = tempoLocalizacaoAnteriorParada - (long) Math.round(periodo);
+
+			if (diferenca > 0) {
+				tempo = tempo + diferenca;
+			}
+		}
+
+		// Calcula tempo entre ultima medida na parada e saida do onibus
+		if (localizacaoPosterior != null) {
+			Point pontoLocalizacaoPosterior = localizacaoPosterior.getGeoPto();
+			Geometry pontoDaParada = ((No) parada.get(parada.size() - 1).getElementoGrafo()).getPonto();
+			// TODO:Calcular essa distância andando sobre a linha e não em linha
+			// reta
+			double distancia = pontoLocalizacaoPosterior.distance(pontoDaParada);
+			double velocidade = localizacaoPosterior.getVelocidade()
+					* Parametros.KILOMETROS_POR_HORA_PARA_METROS_POR_SEGUNDO;
+			double periodo = distancia / velocidade;
+
+			long tempoParadaLocalizacaoPosterior = Math.abs(parada.get(parada.size() - 1).getDataHora().getTime()
+					- localizacaoPosterior.getDataHora().getTime()) / 1000;
+
+			long diferenca = tempoParadaLocalizacaoPosterior - (long) Math.round(periodo);
+
+			if (diferenca > 0) {
+				tempo = tempo + diferenca;
+			}
+		}
+
+		armazenarDadosDeTrafegoDaParada(parada, tempo);
+
+	}
+
+	private void extrairDadosDeTrafegoDeTrecho(List<Localizacao> trecho) {
+		long tempo = 0;
+
+		// Calcula tempo entre pontos do trecho
+		for (int i = 0; i < trecho.size() - 1; i++) {
+			tempo = tempo + Math.abs(trecho.get(i).getDataHora().getTime() - trecho.get(i + 1).getDataHora().getTime())
+					/ 1000;
+		}
+
+		// Calcula tempo entre primeiro ponto do trecho e parada anterior
+		Point primeiroPontoDoTrecho = trecho.get(0).getGeoPto();
+		Geometry paradaAnterior = ((No) trecho.get(0).getElementoGrafo().getAnterior()).getPonto();
+		// TODO:Calcular essa distância andando sobre a linha e não em linha
+		// reta
+		double distancia = primeiroPontoDoTrecho.distance(paradaAnterior);
+		double velocidade = trecho.get(0).getVelocidade() * Parametros.KILOMETROS_POR_HORA_PARA_METROS_POR_SEGUNDO;
+		double periodo = distancia / velocidade;
+		tempo = tempo + (long) Math.round(periodo);
+
+		// Calcula tempo entre ultimo ponto do trecho e parada posterior
+		Point ultimoPontoDoTrecho = trecho.get(trecho.size() - 1).getGeoPto();
+		Geometry paradaPosterior = ((No) trecho.get(trecho.size() - 1).getElementoGrafo().getProximo()).getPonto();
+		// TODO:Calcular essa distância andando sobre a linha e não em linha
+		// reta
+		distancia = ultimoPontoDoTrecho.distance(paradaPosterior);
+		velocidade = trecho.get(trecho.size() - 1).getVelocidade()
+				* Parametros.KILOMETROS_POR_HORA_PARA_METROS_POR_SEGUNDO;
+		periodo = distancia / velocidade;
+		tempo = tempo + (long) Math.round(periodo);
+
+		armazenarDadosDeTrafegoDoTrecho(trecho, tempo);
+
+	}
+
+	private void armazenarDadosDeTrafegoDoTrecho(List<Localizacao> trecho, long tempo) {
+		Localizacao primeiraLocalizacao = trecho.get(0);
+		TempoViagem tempoViagem = new TempoViagem(primeiraLocalizacao.getDataHora(),
+				primeiraLocalizacao.getElementoGrafo().getNome(), tempo);
+		tempoViagemRepository.save(tempoViagem);
+		for (Localizacao localizacao : trecho) {
+			localizacao.setProcessado(true);
+			localizacaoRepository.save(localizacao);
+		}
+	}
+
+	private void armazenarDadosDeTrafegoDaParada(List<Localizacao> parada, Long tempo) {
+		Localizacao primeiraLocalizacao = parada.get(0);
+		TempoViagem tempoViagem = new TempoViagem(primeiraLocalizacao.getDataHora(),
+				primeiraLocalizacao.getElementoGrafo().getNome(), tempo);
+		tempoViagemRepository.save(tempoViagem);
+		for (Localizacao localizacao : parada) {
+			localizacao.setProcessado(true);
+			localizacaoRepository.save(localizacao);
+		}
 	}
 
 	private void encontraElementoGrafoParaCadaLocalizacao(List<Localizacao> localizacoes) {
---
+++ src/main/java/br/unb/cic/extrator/dominio/tempoviagem/TempoViagem.java
@@ -0,0 +1,70 @@
+package br.unb.cic.extrator.dominio.tempoviagem;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "tempo_viagem_preditor")
+public class TempoViagem {
+
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	private long id;
+
+	public TempoViagem(Date dataHora, String nome, Long tempo) {
+		super();
+		this.dataHora = dataHora;
+		this.nome = nome;
+		this.tempo = tempo;
+	}
+
+	@Column(name = "datahora")
+	private Date dataHora;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "tempo")
+	private Long tempo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(Date dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getTempo() {
+		return tempo;
+	}
+
+	public void setTempo(Long tempo) {
+		this.tempo = tempo;
+	}
+
+}
