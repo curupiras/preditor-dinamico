fe3f9e0bbd020357cdf27ab6814c6152f9bd65a4
Curupiras <curupiras@gmail.com>
2018-05-29 21:59:20 -0300

    Criação do pacote preditorhistorico

--- src/main/java/br/unb/cic/extrator/GeradorDeDadosDeTrafego.java
+++ src/main/java/br/unb/cic/extrator/GeradorDeDadosDeTrafego.java
@@ -70,7 +70,7 @@ public class GeradorDeDadosDeTrafego {
 		nos.get(0).setAnterior(arcos.get(arcos.size() - 1));
 	}
 
-	@Scheduled(initialDelay = 0, fixedRate = 3600000)
+	@Scheduled(initialDelay = 3600000, fixedRate = 3600000)
 	public void scheduledTask() {
 		logger.info("Iniciando geração de dados de tráfego.");
 		List<String> listaOnibus = localizacaoRepository.findDistinctOnibus();
