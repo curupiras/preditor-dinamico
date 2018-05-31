cafa5d12892a388aa949ecbc755c6be98f9e1576
Curupiras <curupiras@gmail.com>
2018-05-30 21:00:34 -0300

    Criação do GeradroDeInstances

--- src/main/java/br/unb/cic/preditorhistorico/ConstrutorDeModelos.java
+++ src/main/java/br/unb/cic/preditorhistorico/ConstrutorDeModelos.java
@@ -5,82 +5,121 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.List;
 
-import org.springframework.scheduling.annotation.Scheduled;
+import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import br.unb.cic.extrator.dominio.arco.Arco;
+import br.unb.cic.extrator.dominio.arco.ArcoRepository;
+import br.unb.cic.extrator.dominio.no.No;
+import br.unb.cic.extrator.dominio.no.NoRepository;
 import weka.classifiers.evaluation.NumericPrediction;
 import weka.classifiers.functions.GaussianProcesses;
 import weka.classifiers.timeseries.WekaForecaster;
 import weka.core.Instances;
+import weka.experiment.InstanceQuery;
 
+@Component
 public class ConstrutorDeModelos {
+
+	@Autowired
+	GeradorDeInstances geradorDeInstances;
+
+	@Autowired
+	private ArcoRepository arcoRepository;
+
+	@Autowired
+	private NoRepository noRepository;
 
 	private static final int UM_DIA = 86400000;
 
+	private List<Arco> arcos;
+	private List<No> nos;
+
+	@PostConstruct
+	public void init() {
+		this.arcos = arcoRepository.findAllByOrderByIdAsc();
+		this.nos = noRepository.findAllByOrderByIdAsc();
+	}
+
 	@Scheduled(initialDelay = 0, fixedRate = UM_DIA)
 	public void scheduledTask() {
-		 try {
-		      // path to the Australian wine data included with the time series forecasting
-		      // package
-		      String pathToWineData = weka.core.WekaPackageManager.PACKAGES_DIR.toString()
-		        + File.separator + "timeseriesForecasting" + File.separator + "sample-data"
-		        + File.separator + "wine.arff";
-		 
-		      // load the wine data
-		      Instances wine = new Instances(new BufferedReader(new FileReader(pathToWineData)));
-		 
-		      // new forecaster
-		      WekaForecaster forecaster = new WekaForecaster();
-		 
-		      // set the targets we want to forecast. This method calls
-		      // setFieldsToLag() on the lag maker object for us
-		      forecaster.setFieldsToForecast("Fortified,Dry-white");
-		 
-		      // default underlying classifier is SMOreg (SVM) - we'll use
-		      // gaussian processes for regression instead
-		      forecaster.setBaseForecaster(new GaussianProcesses());
-		 
-		      forecaster.getTSLagMaker().setTimeStampField("Date"); // date time stamp
-		      forecaster.getTSLagMaker().setMinLag(1);
-		      forecaster.getTSLagMaker().setMaxLag(12); // monthly data
-		 
-		      // add a month of the year indicator field
-		      forecaster.getTSLagMaker().setAddMonthOfYear(true);
-		 
-		      // add a quarter of the year indicator field
-		      forecaster.getTSLagMaker().setAddQuarterOfYear(true);
-		 
-		      // build the model
-		      forecaster.buildForecaster(wine, System.out);
-		 
-		      // prime the forecaster with enough recent historical data
-		      // to cover up to the maximum lag. In our case, we could just supply
-		      // the 12 most recent historical instances, as this covers our maximum
-		      // lag period
-		      forecaster.primeForecaster(wine);
-		 
-		      // forecast for 12 units (months) beyond the end of the
-		      // training data
-		      List<List<NumericPrediction>> forecast = forecaster.forecast(12, System.out);
-		 
-		      // output the predictions. Outer list is over the steps; inner list is over
-		      // the targets
-		      for (int i = 0; i < 12; i++) {
-		        List<NumericPrediction> predsAtStep = forecast.get(i);
-		        for (int j = 0; j < 2; j++) {
-		          NumericPrediction predForTarget = predsAtStep.get(j);
-		          System.out.print("" + predForTarget.predicted() + " ");
-		        }
-		        System.out.println();
-		      }
-		 
-		      // we can continue to use the trained forecaster for further forecasting
-		      // by priming with the most recent historical data (as it becomes available).
-		      // At some stage it becomes prudent to re-build the model using current
-		      // historical data.
-		 
-		    } catch (Exception ex) {
-		      ex.printStackTrace();
-		    }
+		try {
+
+			Instances data = geradorDeInstances.getInstancesFromDB(arcos.get(0));
+			System.out.println(data);
+
+			// path to the Australian wine data included with the time series
+			// forecasting
+			// package
+			String pathToWineData = weka.core.WekaPackageManager.PACKAGES_DIR.toString() + File.separator
+					+ "timeseriesForecasting" + File.separator + "sample-data" + File.separator + "wine.arff";
+
+			// load the wine data
+			Instances wine = new Instances(new BufferedReader(new FileReader(pathToWineData)));
+
+			// new forecaster
+			WekaForecaster forecaster = new WekaForecaster();
+
+			// set the targets we want to forecast. This method calls
+			// setFieldsToLag() on the lag maker object for us
+			forecaster.setFieldsToForecast("Fortified,Dry-white");
+
+			// default underlying classifier is SMOreg (SVM) - we'll use
+			// gaussian processes for regression instead
+			forecaster.setBaseForecaster(new GaussianProcesses());
+
+			forecaster.getTSLagMaker().setTimeStampField("Date"); // date time
+																	// stamp
+			forecaster.getTSLagMaker().setMinLag(1);
+			forecaster.getTSLagMaker().setMaxLag(12); // monthly data
+
+			// add a month of the year indicator field
+			forecaster.getTSLagMaker().setAddMonthOfYear(true);
+
+			// add a quarter of the year indicator field
+			forecaster.getTSLagMaker().setAddQuarterOfYear(true);
+
+			// build the model
+			forecaster.buildForecaster(wine, System.out);
+
+			// prime the forecaster with enough recent historical data
+			// to cover up to the maximum lag. In our case, we could just supply
+			// the 12 most recent historical instances, as this covers our
+			// maximum
+			// lag period
+			forecaster.primeForecaster(wine);
+
+			// forecast for 12 units (months) beyond the end of the
+			// training data
+			List<List<NumericPrediction>> forecast = forecaster.forecast(12, System.out);
+
+			// output the predictions. Outer list is over the steps; inner list
+			// is over
+			// the targets
+			for (int i = 0; i < 12; i++) {
+				List<NumericPrediction> predsAtStep = forecast.get(i);
+				for (int j = 0; j < 2; j++) {
+					NumericPrediction predForTarget = predsAtStep.get(j);
+					System.out.print("" + predForTarget.predicted() + " ");
+				}
+				System.out.println();
+			}
+
+			// we can continue to use the trained forecaster for further
+			// forecasting
+			// by priming with the most recent historical data (as it becomes
+			// available).
+			// At some stage it becomes prudent to re-build the model using
+			// current
+			// historical data.
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
---
+++ src/main/java/br/unb/cic/preditorhistorico/GeradorDeInstances.java
@@ -0,0 +1,40 @@
+package br.unb.cic.preditorhistorico;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import br.unb.cic.extrator.dominio.ElementoGrafo;
+import weka.core.Instances;
+import weka.experiment.InstanceQuery;
+
+@Component
+public class GeradorDeInstances {
+
+	@Value("${spring.datasource.password}")
+	private String senha;
+
+	@Value("${spring.datasource.username}")
+	private String usuario;
+
+	@Value("${spring.datasource.url}")
+	private String url;
+
+	public Instances getInstancesFromDB() throws Exception {
+		InstanceQuery query = new InstanceQuery();
+		query.setUsername(usuario);
+		query.setPassword(senha);
+		query.setDatabaseURL(url);
+		query.setQuery("select datahora, tempo from tempo_viagem_preditor order by datahora asc");
+		return query.retrieveInstances();
+	}
+
+	public Instances getInstancesFromDB(ElementoGrafo elementoGrafo) throws Exception {
+		InstanceQuery query = new InstanceQuery();
+		query.setUsername(usuario);
+		query.setPassword(senha);
+		query.setDatabaseURL(url);
+		query.setQuery("select datahora, tempo from tempo_viagem_preditor where nome = '" + elementoGrafo.getNome()
+				+ "' order by datahora asc");
+		return query.retrieveInstances();
+	}
+}
